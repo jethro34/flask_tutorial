1d90fd034971322ff6ca6291d95c5e2f0bd14439
jethro35 <trianaj@oregonstate.edu>
2024-09-29 12:22:42 -0400

    added login page w/ form

--- tutorial1.py
+++ tutorial1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,18 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/new")
-def new():
-    return render_template("new.html")
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form['nm']
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 
 if __name__ == "__main__":
