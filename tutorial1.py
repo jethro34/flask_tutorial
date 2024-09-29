ccc66a7319b12eeb30c8d1c0544505a120129f98
jethro35 <trianaj@oregonstate.edu>
2024-09-29 15:13:46 -0400

    added session

--- tutorial1.py
+++ tutorial1.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 
 @app.route("/")
@@ -12,14 +13,19 @@ def home():
 def login():
     if request.method == "POST":
         user = request.form['nm']
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
         return render_template("login.html")
 
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
