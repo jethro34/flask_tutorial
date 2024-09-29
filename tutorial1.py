b4914311187ee7c320d09c5ba3bab377d13078c8
jethro35 <trianaj@oregonstate.edu>
2024-09-29 08:21:36 -0400

    rendering html functionality

--- tutorial1.py
+++ tutorial1.py
@@ -1,21 +1,11 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello! this is the main page <h1>HELLO</h1>"
-
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello, {name}!"
-
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin"))
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
