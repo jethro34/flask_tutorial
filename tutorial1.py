aa781addcab71098f3165c5ce759d817a9f0983e
jethro35 <trianaj@oregonstate.edu>
2024-09-29 11:43:55 -0400

    added sections and bootstrap

--- tutorial1.py
+++ tutorial1.py
@@ -1,12 +1,17 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=["alys", "lulu", "justi"])
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
