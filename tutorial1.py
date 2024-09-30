a58a0270e9a9158b066102e3436c70ffe98b439b
jethro35 <trianaj@oregonstate.edu>
2024-09-29 21:48:08 -0400

    finished database functionality

--- tutorial1.py
+++ tutorial1.py
@@ -32,6 +32,15 @@ def login():
         session.permanent = True
         user = request.form['nm']
         session["user"] = user
+
+        found_user = User.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = User(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
         flash("Login successful!", "info")
         return redirect(url_for("user"))
     else:
@@ -40,6 +49,11 @@ def login():
             return redirect(url_for("user"))
 
         return render_template("login.html")
+
+
+@app.route("/view")
+def view():
+    return render_template("view.html", values=User.query.all())
 
 
 @app.route("/user", methods=["POST", "GET"])
@@ -51,6 +65,9 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_user = User.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
             flash("Email saved!")
         else:
             if "email" in session:
